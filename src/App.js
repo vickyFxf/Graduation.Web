602f6a7b2c448e20336e7fd49fd39f13bfb37eea
vickyFxf <1004272351@qq.com>
2018-03-14 01:02:08 +0800

    编辑登录页面样式

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Router,Route,hashHistory,IndexRoute} from 'react-router';
 import Root from './js/router/Router';
 
 require('./styles/main.scss');
+require('./styles/lib/antd.min.css');
 ReactDOM.render(
 	<Root/>
 	,
