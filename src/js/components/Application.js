ad2e16ebd2e94f0d17a478090d9aabc3219d1b2e
vickyfan <vickyfan@67tech.cn>
2018-04-08 17:37:20 +0800

    增加退出功能

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -3,79 +3,84 @@
  * Created by vicky on 2018/03/13
  */
 import React from 'react';
-import {Link} from 'react-router';
-import { Layout, Menu, Breadcrumb, Icon,Button} from 'antd';
+import { Link, browserHistory } from 'react-router';
+import { GetUserInfo } from '../services/usersService.js';
+import { Layout, Menu, Breadcrumb, Icon, Button, Modal } from 'antd';
 const { SubMenu } = Menu;
-const { Header, Content, Sider,Footer } = Layout;
+const { Header, Content, Sider, Footer } = Layout;
 
 export default class Application extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            id:sessionStorage.getItem('id'),
-            userName:sessionStorage.getItem('userName'),
-            permissions:sessionStorage.getItem('permissions')
+        this.state = {
+            id: sessionStorage.getItem('id'),
+            userName: sessionStorage.getItem('userName'),
+            permissions: sessionStorage.getItem('permissions')
         }
     }
     render() {
         return (
             <Layout>
                 <Header style={{ height: '10vh', }}>
-                    <img src="/src/img/logo.gif" className="logo left"/>
+                    <img src="/src/img/logo.gif" className="logo left" />
                     <div className="left title">温州医科大学毕业论文管理平台</div>
                     <div className="right">
                         <ul>
-                            <li>欢迎您&nbsp;&nbsp;{this.state.id}&nbsp;{this.state.userName}&nbsp;&nbsp;<a href="javascript:void(0)">退出</a></li>
+                            <li className='currentInfo'>欢迎您
+                                <span>{this.state.id}1406010039</span>
+                                <span>{this.state.userName}范秀芳</span>|
+                                <span onclik={this.logout.bind(this)} className="logout">退出</span>
+                            </li>
                         </ul>
                     </div>
                 </Header>
                 <Layout>
-                    <Sider width={200} style={{ background: '#fff',height: '90vh', }}>
+                    <Sider width={200} style={{ background: '#fff', height: '90vh', }}>
                         <Menu
-                        mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        style={{ height: '100%', borderRight: 0 }}
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{ height: '100%', borderRight: 0 }}
                         >
                             <SubMenu key="sub1" title={<span><Icon type="notification" />通知/通告</span>}>
                                 <Menu.Item key="1">最新消息</Menu.Item>
                                 <Menu.Item key="2">所有通告</Menu.Item>
                             </SubMenu>
-                            {this.state.permissions=="3"?
+                            {this.state.permissions == "3" ?
                                 <SubMenu key="sub2" title={<span><Icon type="team" />用户管理</span>}>
                                     <Menu.Item key="3">管理员</Menu.Item>
                                     <Menu.Item key="4">教师</Menu.Item>
                                     <Menu.Item key="5"><Link to="student">学生</Link></Menu.Item>
                                 </SubMenu>
-                            :""}
-                            {this.state.permissions=="3"||this.state.permissions=="2"?
-                            <SubMenu key="sub4" title={<span><Icon type="appstore-o"/>分类管理</span>}>
-                                {this.state.permissions=="3"?
-                                <Menu.Item key="10">新闻分类</Menu.Item>
-                                :""}
-                                {this.state.permissions=="2"?
-                                <Menu.Item key="11">课题类型</Menu.Item>
-                                :""}
-                                {this.state.permissions=="2"?
-                                <Menu.Item key="12">课题来源</Menu.Item>
-                                :""}
-                            </SubMenu>
-                            :""}
-                            {this.state.permissions=="1"||this.state.permissions=="2"?
+                                : ""}
+                            {this.state.permissions == "3" || this.state.permissions == "2" ?
+                                <SubMenu key="sub4" title={<span><Icon type="appstore-o" />分类管理</span>}>
+                                    {this.state.permissions == "3" ?
+                                        <Menu.Item key="10">新闻分类</Menu.Item>
+                                        : ""}
+                                    {this.state.permissions == "2" ?
+                                        <Menu.Item key="11">课题类型</Menu.Item>
+                                        : ""}
+                                    {this.state.permissions == "2" ?
+                                        <Menu.Item key="12">课题来源</Menu.Item>
+                                        : ""}
+                                </SubMenu>
+                                : ""}
+                            {this.state.permissions == "1" || this.state.permissions == "2" ?
                                 <SubMenu key="sub5" title={<span><Icon type="schedule" />课题管理</span>}>
                                     <Menu.Item key="13"><Link to="subject">所有课题</Link></Menu.Item>
                                     <Menu.Item key="19"><Link to="subject">被选课题</Link></Menu.Item>
-                                    {this.state.permissions=="2"?
-                                    <Menu.Item key="14">待审批课题</Menu.Item>
-                                    :""}
+                                    {this.state.permissions == "2" ?
+                                        <Menu.Item key="14">待审批课题</Menu.Item>
+                                        : ""}
                                 </SubMenu>
-                            :""}
-                            {this.state.permissions=="0"?
+                                : ""}
+                            {this.state.permissions == "0" ?
                                 <SubMenu key="sub6" title={<span><Icon type="schedule" />学生</span>}>
                                     <Menu.Item key="15"><Link to="subjectChoosed">我要选题</Link></Menu.Item>
                                     <Menu.Item key="16">任务</Menu.Item>
                                 </SubMenu>
-                            :""}
+                                : ""}
                             <SubMenu key="sub7" title={<span><Icon type="user" />个人中心</span>}>
                                 <Menu.Item key="17"><Link to="basicInfo">基本信息</Link></Menu.Item>
                                 <Menu.Item key="18"><Link to="changePwd">个人修改密码</Link></Menu.Item>
@@ -94,5 +99,22 @@ export default class Application extends React.Component {
                 </Layout>
             </Layout>
         )
+    }
+    logout() {
+        //先判定是否是第一次登录:初始登录密码为123456加密
+        let data = {};
+        data.id = this.state.id;
+        GetUserInfo(data).then(res => {
+            let mdPwd = hex_md5('123456');
+            if (res.password == mdPwd) {
+                const path = '/changePwd';
+                browserHistory.push(path)
+                // <Modal>
+                //     请先修改密码
+                // </Modal>
+            } else {
+                window.location.href = 'localhost:4000/login.html';
+            }
+        })
     }
 }
